10ad300069a837989e0c691898c9335a98b483b7
Max Steward <maxwell.steward@g.austincc.edu>
2025-01-27 01:22:01 -0600

    deploy script set to 'docs'

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/html-cleanup/', // Ensure this matches your repository name
-});
+  base: mode === "production" ? "/html-cleanup/" : "/",
+  build: {
+    outDir: "docs", // Output folder
+    emptyOutDir: true, // Clear the docs folder before building
+  },
+  server: {
+    host: true, // Allow external access
+    port: 5173,
+  },
+}));
